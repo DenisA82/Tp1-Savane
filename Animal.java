2accc3d08d0dc3ab8d62c48679792bb33bdec516
DenisA82 <91089974+DenisA82@users.noreply.github.com>
2022-03-23 16:52:41 -0400

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,17 @@
+// Fichier :     Animal.java
+// Création:     
+// Auteurs :      
+//
+// Ce code n'est pas protégé par un copyright.
+// 
+// Historique :
+//  Créé pour le cours IFT1025 H22
+//
+
+/**
+   class Animal
+    implements the Prey/Predator relationship
+
+**/
+
+    // TO BE COMPLETED //
